fba9e028193acc41b2cd04cbd93e9251e8f84ea5
Jacky Qin <60870424+qinyong123456@users.noreply.github.com>
2025-06-25 10:24:22 +0800

    Update cutmix.py

--- utils/cutmix.py
+++ utils/cutmix.py
@@ -6,12 +6,16 @@ def rand_bbox(size, lam):
     W = size[2]
     H = size[3]
     cut_rat = np.sqrt(1. - lam)
-    cut_w = np.int(W * cut_rat)
-    cut_h = np.int(H * cut_rat)
+    
+    # 替换 np.int 为 int
+    cut_w = int(W * cut_rat)
+    cut_h = int(H * cut_rat)
 
+    # 随机选择裁剪区域的中心
     cx = np.random.randint(W)
     cy = np.random.randint(H)
 
+    # 计算裁剪区域的边界
     bbx1 = np.clip(cx - cut_w // 2, 0, W)
     bby1 = np.clip(cy - cut_h // 2, 0, H)
     bbx2 = np.clip(cx + cut_w // 2, 0, W)
