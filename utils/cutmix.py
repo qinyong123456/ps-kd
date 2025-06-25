bc3a1e172d88d5192ac7e564c5c03617a72ac245
Jacky Qin <60870424+qinyong123456@users.noreply.github.com>
2025-06-25 10:11:59 +0800

    Update cutmix.py

--- utils/cutmix.py
+++ utils/cutmix.py
@@ -1,38 +1,35 @@
 import torch
-import torchvision.transforms.functional as F
-import random
+import numpy as np
 
-class Cutout(object):
-    def __init__(self, n_holes, length):
-        """
-        初始化 Cutout 类
-        :param n_holes: 要在图像中创建的孔洞数量
-        :param length: 每个孔洞的边长（正方形）
-        """
-        self.n_holes = n_holes
-        self.length = length
+def rand_bbox(size, lam):
+    """生成裁剪区域的边界框"""
+    W = size[2]
+    H = size[3]
+    cut_rat = np.sqrt(1. - lam)
+    cut_w = np.int(W * cut_rat)
+    cut_h = np.int(H * cut_rat)
 
-    def __call__(self, img):
-        """
-        对输入图像应用 Cutout 数据增强
-        :param img: 输入的图像（PIL Image 或 Tensor）
-        :return: 应用 Cutout 后得到的图像
-        """
-        h, w = img.size(1), img.size(2)
-        mask = torch.ones((h, w), dtype=torch.float32)
+    cx = np.random.randint(W)
+    cy = np.random.randint(H)
 
-        for n in range(self.n_holes):
-            y = random.randint(0, h - 1)
-            x = random.randint(0, w - 1)
+    bbx1 = np.clip(cx - cut_w // 2, 0, W)
+    bby1 = np.clip(cy - cut_h // 2, 0, H)
+    bbx2 = np.clip(cx + cut_w // 2, 0, W)
+    bby2 = np.clip(cy + cut_h // 2, 0, H)
 
-            y1 = int(np.clip(y - self.length / 2, 0, h))
-            y2 = int(np.clip(y + self.length / 2, 0, h))
-            x1 = int(np.clip(x - self.length / 2, 0, w))
-            x2 = int(np.clip(x + self.length / 2, 0, w))
+    return bbx1, bby1, bbx2, bby2
 
-            mask[y1: y2, x1: x2] = 0.
+def cutmix(data, targets, alpha=1.0):
+    """实现 CutMix 数据增强"""
+    lam = np.random.beta(alpha, alpha)
+    batch_size = data.size()[0]
+    index = torch.randperm(batch_size)
 
-        mask = mask.expand_as(img)
-        img = img * mask
+    bbx1, bby1, bbx2, bby2 = rand_bbox(data.size(), lam)
+    data[:, :, bbx1:bbx2, bby1:bby2] = data[index, :, bbx1:bbx2, bby1:bby2]
 
-        return img
+    lam = 1 - ((bbx2 - bbx1) * (bby2 - bby1) / (data.size()[-1] * data.size()[-2]))
+    targets_a = targets
+    targets_b = targets[index]
+
+    return data, targets_a, targets_b, lam
